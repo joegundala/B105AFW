61470ee4e2c37ebff61be9b54431ec2ff8ef95f7
joegundala <joegundala89@gmail.com>
2024-07-09 19:27:10 +0530

    changed info
    Signed-off-by: joegundala <joegundala89@gmail.com>

--- page/login_page.py
+++ page/login_page.py
@@ -1,6 +1,8 @@
 import time
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
 class LoginPage:
 
     def __init__(self,driver):
@@ -8,6 +10,7 @@ class LoginPage:
         self.__un=(By.NAME,"username")
         self.__pw=(By.NAME,"password")
         self.__go=(By.NAME,"login-button")
+        self.__errmsg=(By.CSS_SELECTOR,".error")
 
     def set_username(self,un):
         self.driver.find_element(*self.__un).send_keys(un)
@@ -17,4 +20,13 @@ class LoginPage:
 
     def click_go(self):
         self.driver.find_element(*self.__go).click()
+
+    def verify_errmsg_is_displayed(self, wait):
+        try:
+            wait.until(EC.visibility_of_element_located(self.__errmsg))
+            print('Err Msg is displayed')
+            return True
+        except:
+            print('Err Msg is NOT displayed')
+            return False
 
